f6ff21a4df555f31a72c5da14a7934a52cdbe6e2
Adrian Juhl <adrian.juhl@gmail.com>
2025-12-14 18:27:39 +1030

    Initial implementation.

---
+++ .extras/bin/install.sh
@@ -0,0 +1,270 @@
+#!/usr/bin/env bash
+
+# Install the capture_stdout_and_stderr function support script.
+
+usage()
+{
+  cat <<USAGE_TEXT
+Usage:  ${THIS_SCRIPT_NAME}
+            [--install_bin_dir=<dir>]
+            [--capture_script_name=<script_name>]
+            [--capture_script_version=<version>]
+            [--capture_script_version_ref_type=<tags|heads>]
+            [--requires_become=<true|false>]
+            [--dry_run]
+            [--show_diff]
+            [--help | -h]
+            [--script_debug]
+
+Install the capture_stdout_and_stderr function support script.
+
+Available options:
+    --install_bin_dir=<dir>
+        The directory where the script is to be installed.
+        Defaults to "/usr/local/bin".
+    --capture_script_name<script_name>
+        The name that the script and containing folder is to be given.
+        Defaults to "capture_stdout_and_stderr".
+    --capture_script_version=<version>
+        The version of the script to install.
+        Defaults to "0.6.0".
+    --capture_script_version_ref_type<tags|heads>
+        The ref type of the version.
+        Defaults to "tags".
+    --requires_become=<true|false>
+        Is privilege escalation required?
+        Defaults to true.
+    --dry_run
+        Run the role without making changes.
+    --show_diff
+        Run the role in diff mode.
+    --help, -h
+        Print this help and exit.
+    --script_debug
+        Print script debug info.
+USAGE_TEXT
+}
+
+main()
+{
+  initialize
+  parse_script_params "${@}"
+  install_thycotic_cli
+}
+
+install_thycotic_cli()
+{
+  export ANSIBLE_ROLES_PATH="${THIS_SCRIPT_DIRECTORY}/../.ansible/roles/:${HOME}/.ansible/roles/"
+
+  # Install the dependencies of the playbook:
+  ANSIBLE_ROLES_PATH="${HOME}/.ansible/roles/" \
+      ansible-galaxy \
+      role \
+      install \
+      "--role-file=${THIS_SCRIPT_DIRECTORY}/../.ansible/roles/requirements.yml" \
+      --force
+  last_command_return_code="$?"
+  if [ "${last_command_return_code}" -ne 0 ]; then
+    echo >&2 "Error: ansible-galaxy role installations failed."
+    abort_script
+  fi
+
+  ANSIBLE_VERBOSE_ARGUMENT="-v"
+
+  ASK_BECOME_PASS_OPTION=""
+  if [ "${REQUIRES_BECOME}" = "${TRUE_STRING}" ]; then
+    ASK_BECOME_PASS_OPTION="--ask-become-pass"
+  fi
+
+  construct_command_options_array \
+    "playbook_command_options_array" \
+    "${ANSIBLE_CHECK_MODE_ARGUMENT}" \
+    "${ANSIBLE_DIFF_MODE_ARGUMENT}" \
+    "${ANSIBLE_VERBOSE_ARGUMENT}" \
+    "${ASK_BECOME_PASS_OPTION}" \
+    "--inventory=localhost," \
+    "--connection=local" \
+    "--extra-vars=adrianjuhl__shell_capture_stdout_and_stderr__install_bin_dir=${CAPTURE_SCRIPT_INSTALL_BIN_DIR}" \
+    "--extra-vars=adrianjuhl__shell_capture_stdout_and_stderr__script_name=${CAPTURE_SCRIPT_NAME}" \
+    "--extra-vars=adrianjuhl__shell_capture_stdout_and_stderr__version=${CAPTURE_SCRIPT_VERSION}" \
+    "--extra-vars=adrianjuhl__shell_capture_stdout_and_stderr__ref_type=${CAPTURE_SCRIPT_VERSION_REF_TYPE}" \
+    "--extra-vars=local_playbook__install_shell_capture_stdout_and_stderr__requires_become=${REQUIRES_BECOME}" \
+
+  ansible-playbook \
+    "${playbook_command_options_array[@]}" \
+    ${THIS_SCRIPT_DIRECTORY}/../.ansible/playbooks/install.yml
+}
+
+parse_script_params()
+{
+  #echo >&2 "script params (${#}) are: ${@}"
+  # default values of variables set from params
+  CAPTURE_SCRIPT_INSTALL_BIN_DIR="/usr/local/bin"
+  CAPTURE_SCRIPT_NAME="capture_stdout_and_stderr"
+  CAPTURE_SCRIPT_VERSION="0.6.0"
+  CAPTURE_SCRIPT_VERSION_REF_TYPE="tags"
+  REQUIRES_BECOME="${TRUE_STRING}"
+  REQUIRES_BECOME_PARAM=""
+  ANSIBLE_CHECK_MODE_ARGUMENT=""
+  ANSIBLE_DIFF_MODE_ARGUMENT=""
+  SCRIPT_DEBUG_OPTION="${FALSE_STRING}"
+  while [ "${#}" -gt 0 ]
+  do
+    case "${1-}" in
+      --install_bin_dir=*)
+        CAPTURE_SCRIPT_INSTALL_BIN_DIR="${1#*=}"
+        ;;
+      --capture_script_name=*)
+        CAPTURE_SCRIPT_NAME="${1#*=}"
+        ;;
+      --capture_script_version=*)
+        CAPTURE_SCRIPT_VERSION="${1#*=}"
+        ;;
+      --capture_script_version_ref_type=*)
+        CAPTURE_SCRIPT_VERSION_REF_TYPE="${1#*=}"
+        ;;
+      --requires_become=*)
+        REQUIRES_BECOME_PARAM="${1#*=}"
+        ;;
+      --dry_run)
+        ANSIBLE_CHECK_MODE_ARGUMENT="--check"
+        ;;
+      --show_diff)
+        ANSIBLE_DIFF_MODE_ARGUMENT="--diff"
+        ;;
+      --help | -h)
+        usage
+        exit
+        ;;
+      --script_debug)
+        set -x
+        SCRIPT_DEBUG_OPTION="${TRUE_STRING}"
+        ;;
+      -?*)
+        echo >&2 "Error: Unknown parameter: ${1}"
+        echo >&2 "Use --help for usage help"
+        abort_script
+        ;;
+      *) break ;;
+    esac
+    shift
+  done
+  case "${REQUIRES_BECOME_PARAM}" in
+    "true")
+      REQUIRES_BECOME="${TRUE_STRING}"
+      ;;
+    "false")
+      REQUIRES_BECOME="${FALSE_STRING}"
+      ;;
+    "")
+      REQUIRES_BECOME="${TRUE_STRING}"
+      ;;
+    *)
+      echo >&2 "Error: Invalid requires_become param value: ${REQUIRES_BECOME_PARAM}, expected one of: true, false"
+      abort_script
+      ;;
+  esac
+}
+
+construct_command_options_array()
+  # Creates an array as named with the first parameter and populates it with the
+  # non-blank/non-empty values of the remaining parameters.
+  # Parameters:
+  #   ${1}     - the name of the array
+  #   ${2}...  - the values to populate the array with (the blank/empty values will be ignored)
+  # For example:
+  #   construct_command_options_array "command_options_array" "value1" "value2"
+  #   declare -p command_options_array
+  #   echo "command_options_array: ${command_options_array[*]}"
+  #   for value in "${command_options_array[@]}"; do echo "value is: ${value}"; done
+  #   mvn \
+  #     "${command_options_array[@]}"
+{
+  # shellcheck disable=SC2064
+  trap "$(shopt -p extglob)" RETURN  # Restores the extglob shopt when this fuction returns.
+  shopt -s extglob
+  local -n __construct_command_options_array__command_options_array="${1}"
+  shift
+  __construct_command_options_array__command_options_array=()
+  for element in "${@}"
+  do
+    trimmed_element="${element}"
+    trimmed_element="${trimmed_element##+([[:space:]])}" # trim leading whitespace
+    trimmed_element="${trimmed_element%%+([[:space:]])}" # time trailing whitespace
+    if [ -n "${trimmed_element}" ]; then
+      __construct_command_options_array__command_options_array+=("${trimmed_element}")
+    fi
+  done
+}
+
+initialize()
+{
+  set -o pipefail
+  THIS_SCRIPT_PROCESS_ID=$$
+  initialize_abort_script_config
+  initialize_this_script_directory_variable
+  initialize_this_script_name_variable
+  initialize_true_and_false_strings
+}
+
+initialize_this_script_directory_variable()
+{
+  # Determines the value of THIS_SCRIPT_DIRECTORY, the absolute directory name where this script resides.
+  # See: https://www.binaryphile.com/bash/2020/01/12/determining-the-location-of-your-script-in-bash.html
+  # See: https://stackoverflow.com/a/67149152
+  local last_command_return_code
+  THIS_SCRIPT_DIRECTORY=$(cd "$(dirname -- "${BASH_SOURCE[0]}")" || exit 1; cd -P -- "$(dirname "$(readlink -- "${BASH_SOURCE[0]}" || echo .)")" || exit 1; pwd)
+  last_command_return_code="$?"
+  if [ "${last_command_return_code}" -gt 0 ]; then
+    # This should not occur for the above command pipeline.
+    echo >&2 "Error: Failed to determine the value of this_script_directory."
+    abort_script
+  fi
+}
+
+initialize_this_script_name_variable()
+{
+  local path_to_invoked_script
+  local default_script_name
+  path_to_invoked_script="${BASH_SOURCE[0]}"
+  default_script_name=""
+  if grep -q '/dev/fd' <(dirname "${path_to_invoked_script}"); then
+    # The script was invoked via process substitution
+    if [ -z "${default_script_name}" ]; then
+      THIS_SCRIPT_NAME="<script invoked via file descriptor (process substitution) and no default name set>"
+    else
+      THIS_SCRIPT_NAME="${default_script_name}"
+    fi
+  else
+    THIS_SCRIPT_NAME="$(basename "${path_to_invoked_script}")"
+  fi
+}
+
+initialize_true_and_false_strings()
+{
+  # Bash doesn't have a native true/false, just strings and numbers,
+  # so this is as clear as it can be, using, for example:
+  # if [ "${my_boolean_var}" = "${TRUE_STRING}" ]; then
+  # where previously 'my_boolean_var' is set to either ${TRUE_STRING} or ${FALSE_STRING}
+  TRUE_STRING="true"
+  FALSE_STRING="false"
+}
+
+initialize_abort_script_config()
+{
+  # Exit shell script from within the script or from any subshell within this script - adapted from:
+  # https://cravencode.com/post/essentials/exit-shell-script-from-subshell/
+  # Exit with exit status 1 if this (top level process of this script) receives the SIGUSR1 signal.
+  # See also the abort_script() function which sends the signal.
+  trap "exit 1" SIGUSR1
+}
+
+abort_script()
+{
+  echo >&2 "aborting..."
+  kill -SIGUSR1 ${THIS_SCRIPT_PROCESS_ID}
+  exit
+}
+
+# Main entry into the script - call the main() function
+main "${@}"
---
+++ README.md
@@ -0,0 +1,98 @@
+# Ansible role: shell_capture_stdout_and_stderr
+
+Installs [capture-stdout-and-stderr](https://github.com/adrianjuhl/adrianjuhl-shell-capture-stdout-and-stderr), a shell script with a contained function that provides the ability to capture the standard out and standard error of another command.
+
+## Requirements
+
+* This role requires root access by default (unless configured to install into a directory owned by the ansible user - see Role Variables section), so either run it in a playbook with a global `become: true`, or invoke the role with `become: true`.
+
+## Role Variables
+
+Role variables and their defaults.
+
+**version**
+
+    adrianjuhl__shell_capture_stdout_and_stderr__version: "0.6.0"
+
+The version of the script to install.
+
+**ref_type**
+
+    adrianjuhl__shell_capture_stdout_and_stderr__ref_type: "tags"
+
+The ref type within the git repository of the version to be installed.
+Valid values are "tags" or "heads".
+The default of "tags" should be suitable for installing all realease versions of the script as each of these will be given a tag.
+The value "heads" can be used in the case that a version from a branch is to be installed.
+
+**install_bin_dir**
+
+    adrianjuhl__shell_capture_stdout_and_stderr__install_bin_dir: "/usr/local/bin"
+
+The directory where shell_capture_stdout_and_stderr is to be installed.
+
+shell_capture_stdout_and_stderr could alternatively be installed into a user's directory, for example: `adrianjuhl__shell_capture_stdout_and_stderr__install_bin_dir: "{{ ansible_env.HOME }}/.local/bin"`, in which case the role will not need root access.
+
+**script_name**
+
+    adrianjuhl__shell_capture_stdout_and_stderr__script_name: "capture_stdout_and_stderr"
+
+The name to give the script and containing folder.
+
+**create_symbolic_link**
+
+    adrianjuhl__shell_capture_stdout_and_stderr__create_symbolic_link: true
+
+Whether to create a symbolic link from the install bin directory to the installed script in the versioned sub-directory. Boolean true or false value.
+
+## Dependencies
+
+None.
+
+## Example Playbook
+```
+- hosts: "servers"
+  roles:
+    - { role: adrianjuhl.shell_capture_stdout_and_stderr, become: true }
+
+or
+
+- hosts: "servers"
+  tasks:
+    - name: "Install shell_capture_stdout_and_stderr"
+      include_role:
+        name: "adrianjuhl.shell_capture_stdout_and_stderr"
+        apply:
+          become: true
+
+or (install into the user's ~/.local/bin directory)
+
+- hosts: "servers"
+  tasks:
+    - name: "Install shell_capture_stdout_and_stderr"
+      include_role:
+        name: "adrianjuhl.shell_capture_stdout_and_stderr"
+      vars:
+        adrianjuhl__shell_capture_stdout_and_stderr__install_bin_dir: "{{ ansible_env.HOME }}/.local/bin"
+```
+
+## Extras
+
+### Install script
+
+For convenience, a bash script is also supplied that facilitates easy installation of this script on localhost (the script executes ansible-galaxy to install the role and then executes ansible-playbook to run a playbook that includes the shell_capture_stdout_and_stderr role).
+
+The script can be run like this:
+```
+$ git clone git@github.com:adrianjuhl/ansible-role-shell-capture-stdout-and-stderr.git
+$ cd ansible-role-shell-capture-stdout-and-stderr
+$ .extras/bin/install.sh
+```
+
+## License
+
+MIT
+
+## Author Information
+
+[Adrian Juhl](http://github.com/adrianjuhl)
